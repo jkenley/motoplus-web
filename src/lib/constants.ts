9acc0a55a7226259a16e0d769bc00898a2d76888
Kenley Jean <bedjiken@gmail.com>
2024-01-18 00:32:57 -0500

    Update WEB_URL to point to Render URL instead of Vercel URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const WEB_URL = process.env.NODE_ENV === 'production' ? 'https://motoplus-web.vercel.app' : 'http://localhost:3000';
+export const WEB_URL = process.env.NODE_ENV === 'production' ? 'https://motoplus-web.onrender.com' : 'http://localhost:3000';
 export const BASE_API_URL =
   process.env.NODE_ENV === 'production' ? 'https://mbtp-motors-cms.onrender.com' : 'http://localhost:1337';
